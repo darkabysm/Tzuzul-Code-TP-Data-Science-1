dadc54a7b23ede11bdc23c5f9ddba8bdc6a05fe0
Martin Beltramino <martinbeltra2016@gmail.com>
2022-05-01 21:14:46 -0300

    commit inicial

---
+++ main.py
@@ -0,0 +1,4 @@
+# Aquí haremos todo el proceso de limpieza y organización de datos a mostrar
+
+if __name__ == '__main__':
+    print('Hello world')
---
+++ readme.md
@@ -0,0 +1,6 @@
+# TP 1 Bootcamp Tzuzul Code
+
+### Changelog:
+
+#### Commit 1:
+- Agregados los archivos básicos necesarios
